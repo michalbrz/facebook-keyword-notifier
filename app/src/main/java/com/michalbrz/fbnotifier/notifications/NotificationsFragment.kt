6a23323f9008f0c26913cfc90da97ddee899bb5b
Michal Brzezinski <michalbrzother@gmail.com>
2017-06-15 14:20:27 +0200

    Implementing adding keyword when FAB is clicked

--- app/src/main/java/com/michalbrz/fbnotifier/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/michalbrz/fbnotifier/notifications/NotificationsFragment.kt
@@ -1,11 +1,14 @@
 package com.michalbrz.fbnotifier.notifications
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.app.AlertDialog
+import android.view.*
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.EditText
+import android.widget.FrameLayout
 import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager
 import com.beloo.widget.chipslayoutmanager.SpacingItemDecoration
 import com.michalbrz.fbkeywordnotifier.storage.DummyKeywordStorage
@@ -37,7 +40,46 @@ class NotificationsFragment : Fragment(), NotificationsView {
         presenter.init()
 
         // show() must be called when FAB is fully drawn
-        addKeywordButton.viewTreeObserver.addOnGlobalLayoutListener {  addKeywordButton?.show() }
+        addKeywordButton.viewTreeObserver.addOnGlobalLayoutListener { addKeywordButton?.show() }
+        addKeywordButton.setOnClickListener { showAddKeywordDialog() }
+    }
+
+    private fun showAddKeywordDialog() {
+        val (editText, frameLayout) = createAddKeywordDialogLayout()
+
+        val dialog = AlertDialog.Builder(context)
+                .setView(frameLayout)
+                .setTitle(R.string.add_keyword)
+                .setPositiveButton("OK", { _, _ -> addKeyword(editText.text.toString()) })
+                .setNegativeButton("Cancel", { dialog, _ -> dialog.dismiss() })
+                .create()
+        dialog.show()
+        showKeyboard(editText, dialog)
+
+    }
+
+    private fun showKeyboard(editText: EditText, dialog: AlertDialog) {
+        editText.requestFocus()
+        dialog.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+    }
+
+    private fun createAddKeywordDialogLayout(): Pair<EditText, FrameLayout> {
+        val editText = EditText(context)
+        val layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        layoutParams.setMargins(dpToPx(15), 0, dpToPx(15), 0)
+        editText.layoutParams = layoutParams
+        val frameLayout = FrameLayout(context)
+        frameLayout.addView(editText, layoutParams)
+        return Pair(editText, frameLayout)
+    }
+
+    fun dpToPx(dp: Int): Int {
+        return (dp * Resources.getSystem().displayMetrics.density).toInt()
+    }
+
+    private fun addKeyword(keywordToAdd: String) {
+        keywordsAdapter.keywords.add(keywordToAdd)
+        keywordsAdapter.notifyDataSetChanged()
     }
 
     private fun createChipsLayoutManager(): ChipsLayoutManager {
